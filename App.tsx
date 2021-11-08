b6274c610c69f087dd7f70756a546f6b0ebb7491
Oleksii Kucherenko <91497964+Kucherenko-Oleksii@users.noreply.github.com>
2021-11-09 00:26:40 +0200

    Main_File

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text></Text>
+      
     </SafeAreaView>
   );
 }
