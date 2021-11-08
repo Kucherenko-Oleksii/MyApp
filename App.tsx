e20373facb8ed0cf19528e25ec9759930b13b184
Oleksii Kucherenko <91497964+Kucherenko-Oleksii@users.noreply.github.com>
2021-11-09 00:24:03 +0200

    Main_File

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text></Text>
+    </SafeAreaView>
   );
 }
 
