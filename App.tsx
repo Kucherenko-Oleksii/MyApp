597671d4dbca159e21c6aebd8af0f41b1af19836
Oleksii Kucherenko <91497964+Kucherenko-Oleksii@users.noreply.github.com>
2021-11-10 09:41:21 +0200

    Main file

--- App.tsx
+++ App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, ScrollView, StatusBar } from 'react-native';
+import Inputs from './src/components/Inputs';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>To do List</Text>
+      <View style = {styles.view}>
+        <Text style = {styles.text}>To do List</Text>
+        <Inputs />
+      </View>
+      <StatusBar barStyle = 'dark-content' />
+      <ScrollView />
     </SafeAreaView>
   );
 }
@@ -16,4 +22,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  view:
+  {
+    flex: 1,
+    justifyContent: 'flex-start'
+  },
+  text:
+  {
+    fontSize: 30,
+
+  }
 });
